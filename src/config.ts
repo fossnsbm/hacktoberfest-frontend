d117bd5447584477bd6f8abd54463ca68b62763f
z9fr <z9fr@protonmail.com>
2022-10-15 05:22:55 +0530

    leaderboard

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   api_url: "https://api.hacktonight.fossnsbm.org",
   register_closed: true,
-  leaderboard_closed: true,
+  leaderboard_closed: false,
   github_users: "http://example.com",
 };
